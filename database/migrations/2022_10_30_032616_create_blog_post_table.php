b3847038df4fb81e51c0cbbb1fbab666a6c50c6c
HaziqIrfan <wanmuhamadhaziqirfan@gmail.com>
2022-10-30 11:37:18 +0800

    update 2.0

---
+++ database/migrations/2022_10_30_032616_create_blog_post_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blog_post', function (Blueprint $table) {
+            $table->id();
+            $table->varchar('tilte');
+            $table->varchar('body');
+            $table->unsignedBigInteger('user_id');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');//foreign key
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blog_post');
+    }
+};
